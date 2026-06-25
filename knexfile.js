9c63502a5bf0810c4f13f7ed89700515759a1f88
kalmyon <kalkalmyon@gmail.com>
2026-06-25 06:31:19 +0000

    knex.jsを利用したデータベース操作に変更しよう

--- knexfile.js
+++ knexfile.js
@@ -1,46 +1,44 @@
 // Update with your config settings.
-/**
- * @type { Object.<string, import("knex").Knex.Config> }
- */
+
 module.exports = {
 
   development: {
-    client: 'sqlite3',
+    client: "mysql",
     connection: {
-      filename: './dev.sqlite3'
-    }
+      database: "todo_app",
+      user: "root",
+      password: "password",
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
   },
 
   staging: {
-    client: 'postgresql',
+    client: "mysql",
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: "todo_app",
+      user: "root",
+      password: "password",
     },
     pool: {
       min: 2,
       max: 10
     },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
   },
 
   production: {
-    client: 'postgresql',
+    client: "mysql",
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: "todo_app",
+      user: "root",
+      password: "password",
     },
     pool: {
       min: 2,
       max: 10
     },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
   }
 
 };
